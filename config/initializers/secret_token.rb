d0e4abc8db68afe74776c634bb8a1285a2d54800
Brian Jordan <bcjordan@gmail.com>
2011-01-13 10:20:36 -0500

    Initial commit -- empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileBook::Application.config.secret_token = '228473b42a28a16f20395648a0107489adb2c2f8f6852677b8f47e6b2cba452ccbcfd44847a20025c7232d1102eba6ec056dc4960a9c99cb0f8310b6f3809d7a'
