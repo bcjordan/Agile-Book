d0ee0b6fa64a58c1ed32dc361926f29b1815eca2
Brian Jordan <bcjordan@gmail.com>
2011-01-13 13:49:28 -0500

    Playing with file listings

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -2,6 +2,9 @@ class InfoController < ApplicationController
   def about
     $about_views = 0 unless $about_views
     @time = Time.now
+    @files = Dir.glob('*')
+    @files_up = Dir.glob('../*')
+    @files_root = Dir.glob('/*')
   end
 
   def probe
