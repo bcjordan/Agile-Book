caf70ebdefda1283aec9d7582c2ca1c3815fb509
Brian Jordan <bcjordan@gmail.com>
2011-01-13 11:16:00 -0500

    Add some information, simple global variable counters, links between pages

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,8 +1,11 @@
 class InfoController < ApplicationController
   def about
+    $about_views = 0 unless $about_views
+    @time = Time.now
   end
 
   def probe
+    $probe_views = 0 unless $probe_views
   end
 
   def profile
